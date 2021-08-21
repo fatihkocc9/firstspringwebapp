364741ee621bfc04c9518c970a819d10b13f3a5e
fatihkocc9 <fatihkoc@hotmail.it>
2021-08-21 13:44:12 +0300

    First commit

---
+++ src/main/java/com/trendyolfatih/firstspringwebapp/FirstSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.trendyolfatih.firstspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringWebApplication.class, args);
+	}
+
+}
